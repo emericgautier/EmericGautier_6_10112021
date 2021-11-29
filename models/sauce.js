d17ef72b5969b311f8db802d7dd4bdb876f0144e
emericgautier <emericgtr@gmail.com>
2021-11-29 12:32:21 +0100

    define a schema

---
+++ models/sauce.js
@@ -0,0 +1,22 @@
+/* MODEL DE DONNEES SAUCE || Shéma de données d'une sauce */
+
+// import de mongoose
+const mongoose = require ('mongoose');
+
+// creation du schéma de données d'une sauce
+const sauceSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, default: 0},
+    dislikes: { type: Number, default: 0},
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String]  },
+});
+
+// exporte le schéma
+module.exports = mongoose.model('Sauce', sauceSchema);
---
+++ models/user.js
@@ -0,0 +1,18 @@
+/* MODEL DE DONNEES UTILISATEUR || Shéma de données d'un user */
+
+// importations
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+// création du schéma de données d'un user 
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true }, 
+  password: { type: String, required: true }
+});
+
+// pré-valide les informations avant de les enregistrer avec le plugin mongoose-unique-validator
+// pour qu'il n'y est pas plusieurs utilisateurs avec la même adresse mail
+userSchema.plugin(uniqueValidator);
+
+// exporte le schéma
+module.exports = mongoose.model('User', userSchema);
